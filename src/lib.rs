b9de55131ac91a66ef9f283482f08635da999e2b
Sean Wilson <sean.wilson@ghs.com>
2020-05-27 19:15:09 -0700

    Move tests tests dir

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(
      clippy::all,
-     //clippy::cargo,
+     clippy::cargo,
  )]
 #![allow(clippy::as_conversions)]
 #![allow(clippy::print_stdout)]
@@ -250,7 +250,6 @@ impl<'a> DevTreeNode<'a> {
 
     /// Returns the name of the `DevTreeNode` (including unit address tag)
     #[inline]
-    #[must_use]
     pub fn name(&'a self) -> Result<&'a Str, DevTreeError> {
         self.name
     }
@@ -437,96 +436,5 @@ impl<'a> DevTreeProp<'a> {
         }
         // For some reason infinite for loops need unreachable.
         unreachable!();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Str;
-    use core::mem::size_of;
-    use std::fs::{metadata, File};
-    use std::io::Read;
-
-    unsafe fn read_dtb() -> (Vec<u8>, &'static mut [u8]) {
-        let mut file = File::open("test/riscv64-virt.dtb").unwrap();
-        let len = metadata("test/riscv64-virt.dtb").unwrap().len() as usize;
-        let mut vec: Vec<u8> = vec![0u8; len + size_of::<u32>() * 2];
-
-        let (_, buf, _) = vec.align_to_mut::<u32>();
-        let p = buf.as_mut_ptr();
-        let mut buf = std::slice::from_raw_parts_mut(p as *mut u8, len);
-
-        file.read_exact(&mut buf).unwrap();
-        (vec, buf)
-    }
-
-    #[test]
-    fn test_readsize_advice() {
-        unsafe {
-            let (vec, buf) = read_dtb();
-            let size = crate::DevTree::read_totalsize(&buf).unwrap();
-            let buf = &buf[..size];
-            let _blob = crate::DevTree::new(buf).unwrap();
-            std::mem::drop(vec);
-        }
-    }
-
-    #[test]
-    fn reserved_entries_iter() {
-        unsafe {
-            let (vec, buf) = read_dtb();
-            let blob = crate::DevTree::new(buf).unwrap();
-            assert!(blob.reserved_entries().count() == 0);
-            std::mem::drop(vec);
-        }
-    }
-
-    #[test]
-    fn nodes_iter() {
-        unsafe {
-            let (vec, buf) = read_dtb();
-            let blob = crate::DevTree::new(buf).unwrap();
-            for node in blob.nodes() {
-                println!("{}", node.name.unwrap());
-            }
-            assert!(blob.nodes().count() == 27);
-            std::mem::drop(vec);
-        }
-    }
-
-    #[test]
-    fn node_prop_iter() {
-        unsafe {
-            let (vec, buf) = read_dtb();
-            let blob = crate::DevTree::new(buf).unwrap();
-            for node in blob.nodes() {
-                println!("{}", node.name.unwrap());
-                for prop in node.props() {
-                    println!("\t{}", prop.name().unwrap());
-                    if prop.length() == size_of::<u32>() {
-                        //println!("\t\t0x{:x}", prop.get_u32(0).unwrap());
-                    }
-                    if prop.length() > 0 {
-                        let i = prop.get_str_count();
-                        if i.is_ok() {
-                            if i.unwrap() == 0 {
-                                break;
-                            }
-                            let mut vec: Vec<Option<&Str>> = vec![None; i.unwrap()];
-                            prop.get_strlist(&mut vec).unwrap();
-
-                            let mut iter = vec.iter();
-
-                            while let Some(Some(s)) = iter.next() {
-                                print!("\t\t{} ", s);
-                            }
-                            println!();
-                        }
-                    }
-                }
-            }
-            // Wait until the end to drop in since we alias the address.
-            std::mem::drop(vec);
-        }
     }
 }
---
+++ tests/parsing_test.rs
@@ -0,0 +1,97 @@
+extern crate fdt_rs;
+
+use fdt_rs::Str;
+use core::mem::size_of;
+
+use std::path::PathBuf;
+use std::fs::{metadata, File};
+use std::io::Read;
+
+use fdt_rs::DevTree;
+
+unsafe fn read_dtb() -> (Vec<u8>, &'static mut [u8]) {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/riscv64-virt.dtb");
+    println!("{}", d.display());
+
+    let mut file = File::open(&d).unwrap();
+    let len = metadata(&d).unwrap().len() as usize;
+    let mut vec: Vec<u8> = vec![0u8; len + size_of::<u32>() * 2];
+
+    let (_, buf, _) = vec.align_to_mut::<u32>();
+    let p = buf.as_mut_ptr();
+    let mut buf = std::slice::from_raw_parts_mut(p as *mut u8, len);
+
+    file.read_exact(&mut buf).unwrap();
+    (vec, buf)
+}
+
+#[test]
+fn test_readsize_advice() {
+    unsafe {
+        let (vec, buf) = read_dtb();
+        let size = DevTree::read_totalsize(&buf).unwrap();
+        let buf = &buf[..size];
+        let _blob = DevTree::new(buf).unwrap();
+        std::mem::drop(vec);
+    }
+}
+
+#[test]
+fn reserved_entries_iter() {
+    unsafe {
+        let (vec, buf) = read_dtb();
+        let blob = DevTree::new(buf).unwrap();
+        assert!(blob.reserved_entries().count() == 0);
+        std::mem::drop(vec);
+    }
+}
+
+#[test]
+fn nodes_iter() {
+    unsafe {
+        let (vec, buf) = read_dtb();
+        let blob = DevTree::new(buf).unwrap();
+        for node in blob.nodes() {
+            println!("{}", node.name().unwrap());
+        }
+        assert!(blob.nodes().count() == 27);
+        std::mem::drop(vec);
+    }
+}
+
+#[test]
+fn node_prop_iter() {
+    unsafe {
+        let (vec, buf) = read_dtb();
+        let blob = DevTree::new(buf).unwrap();
+        for node in blob.nodes() {
+            println!("{}", node.name().unwrap());
+            for prop in node.props() {
+                println!("\t{}", prop.name().unwrap());
+                if prop.length() == size_of::<u32>() {
+                    //println!("\t\t0x{:x}", prop.get_u32(0).unwrap());
+                }
+                if prop.length() > 0 {
+                    let i = prop.get_str_count();
+                    if i.is_ok() {
+                        if i.unwrap() == 0 {
+                            break;
+                        }
+                        let mut vec: Vec<Option<&Str>> = vec![None; i.unwrap()];
+                        prop.get_strlist(&mut vec).unwrap();
+
+                        let mut iter = vec.iter();
+
+                        while let Some(Some(s)) = iter.next() {
+                            print!("\t\t{} ", s);
+                        }
+                        println!();
+                    }
+                }
+            }
+        }
+        // Wait until the end to drop in since we alias the address.
+        std::mem::drop(vec);
+    }
+}
