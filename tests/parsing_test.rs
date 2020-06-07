594c587591ead001c53dd020a2957eb1a2a70c1e
Sean Wilson <sean.wilson@ghs.com>
2020-06-06 17:10:21 -0700

    Replace crazy device tree read code with a simple aligned include_bytes

--- tests/parsing_test.rs
+++ tests/parsing_test.rs
@@ -9,62 +9,41 @@ use std::path::PathBuf;
 
 use fdt_rs::DevTree;
 
-unsafe fn read_dtb() -> (Vec<u8>, &'static mut [u8]) {
-    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    d.push("tests/riscv64-virt.dtb");
-    println!("{}", d.display());
-
-    let mut file = File::open(&d).unwrap();
-    let len = metadata(&d).unwrap().len() as usize;
-    let mut vec: Vec<u8> = vec![0u8; len + size_of::<u32>() * 2];
-
-    let (_, buf, _) = vec.align_to_mut::<u32>();
-    let p = buf.as_mut_ptr();
-    let mut buf = std::slice::from_raw_parts_mut(p as *mut u8, len);
-
-    file.read_exact(&mut buf).unwrap();
-    (vec, buf)
-}
+#[repr(align(4))] struct _Wrapper<T>(T);
+pub const FDT: &[u8] = &_Wrapper(*include_bytes!("riscv64-virt.dtb")).0;
 
 #[test]
 fn test_readsize_advice() {
     unsafe {
-        let (vec, buf) = read_dtb();
-        let size = DevTree::read_totalsize(&buf).unwrap();
-        let buf = &buf[..size];
-        let _blob = DevTree::new(buf).unwrap();
-        std::mem::drop(vec);
+        let size = DevTree::read_totalsize(FDT).unwrap();
+        assert!(size == FDT.len());
+        let _blob = DevTree::new(FDT).unwrap();
     }
 }
 
 #[test]
 fn reserved_entries_iter() {
     unsafe {
-        let (vec, buf) = read_dtb();
-        let blob = DevTree::new(buf).unwrap();
+        let blob = DevTree::new(FDT).unwrap();
         assert!(blob.reserved_entries().count() == 0);
-        std::mem::drop(vec);
     }
 }
 
 #[test]
 fn nodes_iter() {
     unsafe {
-        let (vec, buf) = read_dtb();
-        let blob = DevTree::new(buf).unwrap();
+        let blob = DevTree::new(FDT).unwrap();
         for node in blob.nodes() {
             println!("{}", node.name().unwrap());
         }
         assert!(blob.nodes().count() == 27);
-        std::mem::drop(vec);
     }
 }
 
 #[test]
 fn node_prop_iter() {
     unsafe {
-        let (vec, buf) = read_dtb();
-        let blob = DevTree::new(buf).unwrap();
+        let blob = DevTree::new(FDT).unwrap();
         for node in blob.nodes() {
             println!("{}", node.name().unwrap());
             for prop in node.props() {
@@ -91,8 +70,6 @@ fn node_prop_iter() {
                 }
             }
         }
-        // Wait until the end to drop in since we alias the address.
-        std::mem::drop(vec);
     }
 }
 
@@ -100,8 +77,7 @@ fn node_prop_iter() {
 fn find_first_compatible() {
     use fdt_rs::DevTreeItem;
     unsafe {
-        let (vec, buf) = read_dtb();
-        let blob = DevTree::new(buf).unwrap();
+        let blob = DevTree::new(FDT).unwrap();
         // TODO
         assert!(blob.find(|item| 
             match item {
@@ -109,6 +85,5 @@ fn find_first_compatible() {
                 _ => false,
             }
         ).is_some());
-        std::mem::drop(vec);
     }
 }
